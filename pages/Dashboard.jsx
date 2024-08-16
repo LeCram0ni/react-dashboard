1d4ec54a0493201bd40d6c6c6793b4aab86d456a
lecram <15247414+LeCram0ni@users.noreply.github.com>
2024-08-16 19:48:49 +0200

    first push

---
+++ pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+    return (
+        <>
+            <main class="container">
+                <h1>Hello world!</h1>
+                <button>Hello</button>
+            </main>
+        </>
+    );
+}
